97bb37edb7a0c0d2c6e4d07df549e7d4fbd0cc4f
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-03-07 10:33:29 +0200

    Feat: Created first part of Teplate design

---
+++ src/app/plugins/available-plugins/available-plugins.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AvailablePluginsComponent } from './available-plugins.component';
+
+describe('AvailablePluginsComponent', () => {
+  let component: AvailablePluginsComponent;
+  let fixture: ComponentFixture<AvailablePluginsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AvailablePluginsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AvailablePluginsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/plugins/available-plugins/available-plugins.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-available-plugins',
+  templateUrl: './available-plugins.component.html',
+  styleUrls: ['./available-plugins.component.css']
+})
+export class AvailablePluginsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
