37981fcfc8f98de1ea0162ebcd29526399392cbe
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-03-17 23:10:37 +0200

    Changes for Heroku

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 8000;
 const cors = require('cors');
-const server = require('http').Server(app);
 
 app.use(cors());
-app.use(express.static('./src'));
+app.use(express.static('./dist/geo-make-it-studio'));
 
 app.get('/*', function (req, res){
-    res.sendFile(path.join(__dirname, './src/index.html'));
-    console.log('path', path.join(__dirname, 'src', 'index.html'))
+    res.sendFile(path.join(__dirname, './dist/geo-make-it-studio/index.html'));
 });
 
-server.listen(port, function() {
-    console.log("App running on port " + port);
-})
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
