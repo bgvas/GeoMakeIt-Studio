c9a21177ee71d067518e31d5fc7d6b93d50d59cb
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-03-17 22:43:15 +0200

    Fix: Changes in package.json

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 8000;
 const cors = require('cors');
+const server = require('http').Server(app);
 
 app.use(cors());
 app.use(express.static('./src'));
 
 app.get('/*', function (req, res){
     res.sendFile(path.join(__dirname, './src/index.html'));
-    console.log('path', path.join(__dirname, './src/index.html'))
+    console.log('path', path.join(__dirname, 'src', 'index.html'))
 });
 
-app.listen(process.env.PORT || 8080);
+server.listen(port, function() {
+    console.log("App running on port " + port);
+})
