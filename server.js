1c420f0eb2b595212d230a2d89d313dfff32581b
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-03-18 13:34:44 +0200

    Feat: Reinstalled npm modules

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const path = require('path');
 const cors = require('cors');
 
 app.use(cors());
-app.use(express.static('./dist/geo-make-it-studio'));
+
+app.use(express.static(path.join(__dirname, '/dist')));
 
 app.get('/*', function (req, res){
-    res.sendFile(path.join(__dirname, './dist/geo-make-it-studio/index.html'));
+    res.sendFile(__dirname + '/dist/index.html');
 });
+
+
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
