f94ba27963e07b87f11220eea3a779fdae331751
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-04-01 23:40:06 +0300

    Feat: added login and auth functionality

---
+++ src/app/classes/auth/auth-credentials.spec.ts
@@ -0,0 +1,7 @@
+import { AuthCredentials } from './auth-credentials';
+
+describe('AuthCredentials', () => {
+  it('should create an instance', () => {
+    expect(new AuthCredentials()).toBeTruthy();
+  });
+});
