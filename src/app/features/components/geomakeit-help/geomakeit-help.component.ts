bfbfc950a9de863ed2336c140a01a9865882c306
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-04-27 17:34:31 +0300

    Feat: Added dropdown menus on header bar

---
+++ src/app/features/components/geomakeit-help/geomakeit-help.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-geomakeit-help',
+  templateUrl: './geomakeit-help.component.html',
+  styleUrls: ['./geomakeit-help.component.css']
+})
+export class GeomakeitHelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
