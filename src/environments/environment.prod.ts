339200b63469695dbb22244cd96ca94bee6e8b54
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-06-17 19:26:46 +0300

    Fix: Added token in production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
    'production': true,
-   'apiUrl': 'https://api.geomakeit.com/v1/'
+   'apiUrl': 'https://api.geomakeit.com/v1/',
+   'token': '1|XxqPWqgRFk9yj9B7KKmwRBOFziaeiaKsHYb235HA'
 };
