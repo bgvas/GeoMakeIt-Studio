8e65941d693ae100377225795947a96ec73ce26b
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-07-06 10:07:27 +0300

    Changes in env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
    'production': true,
    'apiUrl': 'https://api.geomakeit.com/v1/',
-   'myApiUrl': 'https://geomakeit-studio-be.herokuapp.com/api/v1/',
+   'myApiUrl': 'https://geomakeit-be.herokuapp.com/api/v1/',
    'token': '1|XxqPWqgRFk9yj9B7KKmwRBOFziaeiaKsHYb235HA'
 };
