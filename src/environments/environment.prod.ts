4b9c311dbc4ee533d2e3b3c4a82858636c44ce53
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-07-08 17:16:33 +0300

    Fix: Changed the v1 and v2 urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
    'production': true,
    'v1Url': 'https://api.geomakeit.com/v1/',
    'v2Url': 'https://localhost:8000/api/v2/',
-   'token': '1|XxqPWqgRFk9yj9B7KKmwRBOFziaeiaKsHYb235HA'
+   'v1Token': '1|XxqPWqgRFk9yj9B7KKmwRBOFziaeiaKsHYb235HA'
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,5 @@ export const environment = {
   'production': false,
   'v1Url': 'https://api.geomakeit.com/v1/',
   'v2Url': 'https://localhost:8000/api/v2/',
-  'token': '1|XxqPWqgRFk9yj9B7KKmwRBOFziaeiaKsHYb235HA'
+  'v1Token': '1|XxqPWqgRFk9yj9B7KKmwRBOFziaeiaKsHYb235HA'
 };
