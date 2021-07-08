522495e8d6d3fae58aaafbc923e5cfc55fcbcc55
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-07-08 16:54:32 +0300

    Merge branch 'dev-master' of https://github.com/Gnosis-Projects/geomakeit_studio into dev-master
    # Conflicts:
    #	src/environments/environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 export const environment = {
   'production': false,
   'apiUrl': 'https://api.geomakeit.com/v1/',
-  'myApiUrl': 'https://geomakeit-studio-be.herokuapp.com/api/v1/',
+  'myApiUrl': 'https://localhost:8000/api/v2/',
   'token': '1|XxqPWqgRFk9yj9B7KKmwRBOFziaeiaKsHYb235HA'
 };
