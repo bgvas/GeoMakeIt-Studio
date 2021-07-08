8c00f226c3f2600e8c032665b4f98fbfd19a86d2
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-07-08 17:09:56 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   'production': false,
-  'apiUrl': 'https://api.geomakeit.com/v1/',
-  'myApiUrl': 'https://geomakeit-be.herokuapp.com/api/v2/',
+   'v1Url': 'https://api.geomakeit.com/v1/',
+   'v2Url': 'https://geomakeit-be.herokuapp.com/api/v2/',
   'token': '1|XxqPWqgRFk9yj9B7KKmwRBOFziaeiaKsHYb235HA'
 };
