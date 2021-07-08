da0ecd2d12b597df99d562a10ffc92a7d2f32a6a
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-07-08 17:07:41 +0300

    Fix: Changed the v1 and v2 urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
    'production': true,
-   'apiUrl': 'https://api.geomakeit.com/v1/',
-   'myApiUrl': 'https://geomakeit-be.herokuapp.com/api/v2/',
+   'v1Url': 'https://api.geomakeit.com/v1/',
+   'v2Url': 'https://localhost:8000/api/v2/',
    'token': '1|XxqPWqgRFk9yj9B7KKmwRBOFziaeiaKsHYb235HA'
 };
 
