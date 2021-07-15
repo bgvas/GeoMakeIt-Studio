0fd8272cf5d219f7b84f6aa6dfe7e612351b9dfe
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-07-15 21:02:13 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
-   'production': true,
-   'v1Url': 'https://api.geomakeit.com/v1/',
-   'v2Url': 'http://localhost:8000/api/v2/',
-   'v1Token': '1|XxqPWqgRFk9yj9B7KKmwRBOFziaeiaKsHYb235HA',
-   'timeZone': '+0600' // Greece //
+   'production': false,
+  'v1Url': 'https://api.geomakeit.com/v1/',
+  'v2Url': 'http://176.223.139.84:8080/be/api/v2/',
+  'v1Token': '1|XxqPWqgRFk9yj9B7KKmwRBOFziaeiaKsHYb235HA',
+  'timeZone': '+0600' // Greece //
 };
 
