64feb427a0fae17ea1e17326f3d179a0170679b2
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-09-20 19:37:32 +0300

    Feat: added various features

---
+++ src/app/gamePlugins/game-plugins.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-plugins',
+  templateUrl: './game-plugins.component.html',
+  styleUrls: ['./game-plugins.component.css']
+})
+export class GamePluginsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
