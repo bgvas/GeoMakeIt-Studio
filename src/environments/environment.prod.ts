5b6b5f116a17cbd531348ebd151880c6b2b4d56c
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-10-07 10:51:04 +0300

    Fix: environment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
    'production': true,
-   //'be_Url': 'http://176.223.139.84/api/v1/',
+   'be_Url': 'http://176.223.139.84/api/v1/',
    'base_Fe_Url': 'http://176.223.139.84:8080/studio/#/',
-   'be_Url': 'https://api.geomakeit.com/v1/',
+   //'be_Url': 'https://api.geomakeit.com/v1/',
    'timeZone': '+0600', // Greece //
    'password_pattern': '^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$' // Minimum 8 characters, at least one letter and one number
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   'production': false,
-  //'be_Url': 'http://176.223.139.84/api/v1/',
-  'be_Url': 'https://api.geomakeit.com/v1/',
+  'be_Url': 'http://176.223.139.84/api/v1/',
+  //'be_Url': 'https://api.geomakeit.com/v1/',
   'base_Fe_Url': 'http://176.223.139.84:8080/studio/#/',
   'timeZone': '+0600', // Greece //
   'password_pattern': '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])*.{8,}$' // Minimum 8 characters, at least one upper, one lower and one number
